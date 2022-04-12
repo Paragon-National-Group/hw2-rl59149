3dd47646395ddb605e6acae19f8adc8d2cbcc045
rl59149 <102943950+rl59149@users.noreply.github.com>
2022-04-12 12:00:48 -0500

    a

---
+++ PNG_HW2.py
@@ -0,0 +1,110 @@
+# Paragon National Group Spring 2022
+# Computer Science / Data Science Track
+# HW 2
+
+# Assigned: April 7, 2022
+# Due: April 12, 2022 11:59pm
+
+# This is the second programming assignment of the computer science track.
+# For each function, a skeleton is provided, where your job is to fill the *TODO* out.
+# The questions will cover the topics learned in the second lecture, which entailed
+# recursive functions, for and while loops, string indexing, and linked lists.
+
+# This function takes a natural number as an argument (so it can't be negative) 
+# and returns a boolean value of true or false that shows whether the inputted 
+# number is a palindrome or not. A palindrome is something that reads the same 
+# backward as forward (i.e. '2002'). Use a for or while for the implementation 
+# of this function.
+# Ex) iterative_palindrome(1) -> true
+#     iterative_palindrome(12) -> false
+#     iterative_palindrome(2468642) -> true
+def iterative_palindrome(n):
+    s = str(n)
+
+    for i in range(len(s) // 2):
+        if s[i] != s[len(s) - 1 - i]:
+            return False
+
+    return True
+
+# This function is the same as iterative_palindrome() except instead of using a
+# loop, implement the function in a recursive way.
+# Ex) recursive_palindrome(1) -> true
+#     recursive_palindrome(12) -> false
+#     recursive_palindrome(2468642) -> true
+def recursive_palindrome(n):
+    if not n:
+        return True
+
+    s = str(n)
+    if s[0] != s[len(s) - 1]:
+        return False
+
+    return recursive_palindrome(s[1 : len(s) - 1])
+
+# Helper function for sum_factorials(). Takes a number as an argument and returns
+# the factorial of that number.
+# Ex) factorial(1) -> 1
+#     factorial(4) -> 24
+#     factorial(7) -> 5040
+def factorial(n):
+    res = 1
+
+    for i in range(2, n + 1):
+        res *= i
+
+    return res
+
+# Helper function for sum_factorials(). Takes a number as an argument and returns
+# a boolean value of true or false that shows whether the inputted number is a 
+# prime or not.
+# Ex) is_prime(7) -> true
+#     is_prime(12) -> false
+#     is_prime(23) -> true
+def is_prime(n):
+    if n < 2:
+        return False
+
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+
+        i += 1
+
+    return True
+
+# Function that creates a node to help testing of sum_factorials()
+class Node:
+    def __init__(self, content): 
+        self.content = content
+        self.next = next
+
+# Function to insert a node to the beginning of a linked list
+def push(head_ptr, new_content):
+    new_node = Node(0)
+    new_node.content = new_content
+    new_node.next = (head_ptr)
+    (head_ptr) = new_node
+    return head_ptr
+
+# This function takes a pointer to the head node of the linked list as an argument
+# and returns the sum of factorials of prime numbers in the linked list. Above are
+# two helper functions for this that are highly recommended.
+# Ex) lst = None
+#     lst = push(lst, 2)
+#     lst = push(lst, 14)
+#     lst = push(lst, 5)
+#     lst = push(lst, 3)
+#     lst = push(lst, 6)
+#     sum_factorials(lst) -> 128
+def sum_factorials(head_ptr):
+    res = 0
+
+    while head_ptr:
+        if is_prime(head_ptr.content):
+            res += factorial(head_ptr.content)
+
+        head_ptr = head_ptr.next
+
+    return res
